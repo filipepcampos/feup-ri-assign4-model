3a11956b72198c9571aedc4944910d57fff0d5fd
Filipe Campos <filipepcampos24@gmail.com>
2023-12-15 19:54:58 +0000

    chore: remove debug print

--- utils/ducky/loss.py
+++ utils/ducky/loss.py
@@ -50,7 +50,6 @@ class ComputeLoss:
             tobj = torch.zeros(pi.shape[:4], dtype=pi.dtype, device=self.device)  # target obj
 
             n = b.shape[0]  # number of targets
-            # print(n, indices[i])
             if n:
                 # pxy, pwh, _, pcls = pi[b, a, gj, gi].tensor_split((2, 4, 5), dim=1)  # faster, requires torch 1.8.0
                 pxy, pwh, _, pcls = pi[b, a, gj, gi].split((2, 2, 1, self.nc), 1)  # target-subset of predictions
