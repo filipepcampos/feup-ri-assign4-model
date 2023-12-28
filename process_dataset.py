ad594c3575ed7a1c6fff0f44327b2062f24f2f1f
Filipe Campos <filipepcampos24@gmail.com>
2023-12-28 13:43:05 +0000

    chore: update process_dataset.py

--- process_dataset.py
+++ process_dataset.py
@@ -1,5 +1,6 @@
 import os
 import math
+import random
 
 def get_distance_class(distance: float):
     if distance < 0.1:
@@ -24,7 +25,7 @@ def get_angle_class(angle: float): # TODO: This is not correct
     return 4
 
 def convert_line(line: str):
-    ['1 445 139 36 24  -1.4480862438197035 0.0 0.027528700497758685 0.413120234994226']
+    # ['1 445 139 36 24  -1.4480862438197035 0.0 0.027528700497758685 0.413120234994226']
     label, x, y, w, h, relative_x, relative_y, relative_z, angle = line.split(" ")
 
     center_x = float(x) + float(w) / 2.0
@@ -39,16 +40,16 @@ def convert_line(line: str):
     angle_class = get_angle_class(float(angle))
     return f"{label} {x} {y} {w} {h} {distance_class} {angle_class}\n"
     
-def convert_dir(input_dir, output_dir, split):
+def convert_dir(input_dir, output_dir):
     # Copy imgs to output dir
-    input_imgs = os.path.join(root_dir_path, split, "images")
-    output_imgs = os.path.join(output_dir_path, split, "images")
+    input_imgs = os.path.join(root_dir_path, "images")
+    output_imgs = os.path.join(output_dir_path, "images")
     os.makedirs(output_imgs, exist_ok=True)
     os.system(f"cp -r {input_imgs}/* {output_imgs}")
 
     # Convert labels
-    input_dir = os.path.join(root_dir_path, split, "labels")
-    output_dir = os.path.join(output_dir_path, split, "labels")
+    input_dir = os.path.join(root_dir_path, "labels")
+    output_dir = os.path.join(output_dir_path, "labels")
     os.makedirs(output_dir, exist_ok=True)
     
     for file_name in os.listdir(input_dir):
@@ -60,8 +61,30 @@ def convert_dir(input_dir, output_dir, split):
                 for line in lines:
                     output_file.write(convert_line(line))
 
-root_dir_path = "../datasets/duckietown2"
-output_dir_path = "../datasets/duckietown2_processed"
+def split_valid(input_dir, output_dir, valid_ratio=0.2):
+    input_imgs = os.path.join(input_dir, "images")
+    output_imgs = os.path.join(output_dir, "images")
+    os.makedirs(output_imgs, exist_ok=True)
+    input_labels = os.path.join(input_dir, "labels")
+    output_labels = os.path.join(output_dir, "labels")
+    os.makedirs(output_labels, exist_ok=True)
 
-convert_dir(root_dir_path, output_dir_path, "train")
-convert_dir(root_dir_path, output_dir_path, "valid")
+    file_names = os.listdir(input_imgs)
+    random.shuffle(file_names)
+
+    for file_name in file_names[:int(len(file_names)*valid_ratio)]: # Move valid_ratio of files to valid dir
+        file_path = os.path.join(input_imgs, file_name)
+        output_file_path = os.path.join(output_imgs, file_name)
+        os.system(f"mv {file_path} {output_file_path}")
+
+        file_path = os.path.join(input_labels, file_name).replace(".png", ".txt")
+        output_file_path = os.path.join(output_labels, file_name)
+        os.system(f"mv {file_path} {output_file_path}")
+
+root_dir_path = "../datasets/duckietown2"
+output_dir_path = "../datasets/duckietown2_processed/train"
+
+convert_dir(root_dir_path, output_dir_path)
+
+random.seed(1)
+split_valid(output_dir_path, "../datasets/duckietown2_processed/valid")
